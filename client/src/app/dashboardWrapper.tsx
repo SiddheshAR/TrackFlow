aad48c8b11e1e8a2366e381420771a5da29b405a
SiddheshAR <siddheshfr@gmail.com>
2025-07-19 23:34:44 +0530

    Setups and configs

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function DashboardWrapper() {
+  return (
+    <div className='flex flex-row'>
+        {/* Sidebar */}
+        <div className=''>
+
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default DashboardWrapper
